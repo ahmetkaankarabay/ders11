883bd06ec0936a109b2dc62908769da77d0d70a8
ahmetkaankarabay <ahmetkaankarabay8@gmail.com>
2025-01-07 21:23:02 +0300

    Create sozluk.py

---
+++ sozluk.py
@@ -0,0 +1,8 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+if word in meme_dict.keys():
+    # Kelime eşleşiyorsa ne yapmalıyız?print(meme_dict[word])
+else:
+    # Kelime eşleşmiyorsa ne yapmalıyız?
